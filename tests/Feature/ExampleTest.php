0b2c16819dcbd29bafda52e093a94620d3a5eb29
JamelNefzi <djameloter@gmail.com>
2018-11-30 21:00:55 +0100

    add exemple unit test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class ExampleTest extends TestCase
 {
@@ -18,4 +20,32 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $response = $this->withHeaders([
+            'X-Header' => 'Value',
+        ])->json('POST', '/user', ['name' => 'Sally']);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson([
+                'created' => true,
+            ]);
+    }
+
+    public function testApplication()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+                         ->withSession(['foo' => 'bar'])
+                         ->get('/');
+    }
+
 }
